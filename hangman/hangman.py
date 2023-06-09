6042309762fbd208e6d30dbd8cf128d7eeab9f2e
Ryan Matlock <ryan.matlock@gmail.com>
2023-06-09 02:36:14 -0700

    :recycle: clean up code using match/case and list comprehensions

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -2,65 +2,55 @@
 
 Implement a game of hangman using Functional Reactive Programming."""
 
-from typing import List, TypeVar, Sequence
-
-
-T = TypeVar('T')
-
-# Game status categories
 # Change the values as you see fit
 STATUS_WIN = 'win'
 STATUS_LOSE = 'lose'
 STATUS_ONGOING = 'ongoing'
 
 
-def _get_all_indices(obj: T, seq: Sequence) -> List[int]:
-    """Get the indices of all elements in seq equal to obj."""
-    # stolen from https://stackoverflow.com/a/28182579/2677392
-    return [index for index, value in enumerate(seq) if value == obj]
-
-
-def _replace_at_indices(
-        xs: List[T], replacement: T, indices: List[int]) -> List[T]:
-    """Replace the elements of xs with replacement at the given indices."""
-    for index in indices:
-        xs[index] = replacement
-    return xs
-
-
 class Hangman:
     """Create a word for a new game of hangman."""
-    def __init__(self, word):
-        self.remaining_guesses = 9
+
+    def __init__(self, word: str, max_guesses: int = 9) -> None:
+        self.remaining_guesses = max_guesses
         self.status = STATUS_ONGOING
         self.word = word
         self.guesses = []
 
-    def guess(self, char):
-        """Guess another letter."""
-        if self.get_status() == STATUS_ONGOING and\
-           self.remaining_guesses >= 0:
-            if char in self.word and char not in self.guesses:
+    def guess(self, char: str) -> str:
+        """Guess another char, and return the masked word. [Note: returning the
+        masked word wasn't specified, but it makes sense when using this
+        interactively.]"""
+        match self.get_status(), char, self.remaining_guesses:
+            case 'win' | 'lose', _, _:
+                raise ValueError("The game has already ended.")
+            case _, char, _ if char in self.word and char not in self.guesses:
+                # note: guessing a letter correctly (the first time) doesn't
+                # cost a turn
                 self.guesses.append(char)
-                if self.get_masked_word() == self.word:
+                if self.word == self.get_masked_word():
                     self.status = STATUS_WIN
-            elif self.remaining_guesses == 0:
-                self.status = STATUS_LOSE
-            else:
+            case _, _, remaining if remaining > 0:
                 self.remaining_guesses -= 1
-        else:
-            raise ValueError("The game has already ended.")
+            case _, _, 0:
+                self.status = STATUS_LOSE
 
-    def get_masked_word(self):
+        return str(
+            f'word: {self.get_masked_word()!r} '
+            f'status: {self.get_status()}'
+            # f' ({self.remaining_guesses} guesses left)'
+        )
+
+    def get_masked_word(self) -> str:
         """Return the word with the correctly guessed letters displayed."""
-        masked_list = ['_' for letter in self.word]
-        # only unmask single letter replacements
-        for letter in [guess for guess in self.guesses if len(guess) == 1]:
-            _replace_at_indices(
-                masked_list, letter, _get_all_indices(letter, self.word))
-        masked_word = ''.join(masked_list)
-        return masked_word
+        letter_guesses = [
+            guess for guess in self.guesses if len(guess) == 1
+        ]
+        return ''.join(
+            [letter if letter in letter_guesses else '_'
+             for letter in self.word]
+        )
 
-    def get_status(self):
+    def get_status(self) -> str:
         """Return the status of the game."""
         return self.status
