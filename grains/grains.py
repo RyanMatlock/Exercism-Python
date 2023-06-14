151f85e3cd1209cd7215a42d94e0d1ba5be00daa
Ryan Matlock <ryan.matlock@gmail.com>
2023-06-14 14:07:53 -0700

    :white_check_mark: pass all tests

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,18 @@
-def square(number):
-    pass
+CHESSBOARD_MIN = 1
+CHESSBOARD_MAX = 64
+
+def square(number: int) -> int:
+    """Return the number of grains on the chessboard square given by number."""
+    if CHESSBOARD_MIN <= number <= CHESSBOARD_MAX:
+        return 2 ** (number - 1)
+    else:
+        raise ValueError(
+            f"square must be between {CHESSBOARD_MIN} and {CHESSBOARD_MAX}"
+        )
 
 
-def total():
-    pass
+def total() -> int:
+    """Return the total number of grains on the chessboard."""
+    return sum(
+        [square(n) for n in range(CHESSBOARD_MIN, CHESSBOARD_MAX + 1)]
+    )
